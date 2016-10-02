c9e70004365221e23469cdbac7b360831a618b98
Brandon <byh2108@columbia.edu>
2016-10-02 17:00:19 -0400

    Dummy Order Checkboxes Created
    Menu items are selectable checkboxes, but doesn’t do anything yet.

--- click_eat/menu/views.py
+++ click_eat/menu/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Item
+from django.template import loader
+from django.urls import reverse
+
 
 def index(request):
     item_list = Item.objects.all()
-    output = ''
-    for i in item_list:
-        output = output + i.item_name + ' ' + str(i.item_price) + '<br/>'
-    return HttpResponse(output)
+    template = loader.get_template('menu/index.html')
+    context = {
+        'item_list':item_list,
+    }
+    return HttpResponse(template.render(context, request))
+
