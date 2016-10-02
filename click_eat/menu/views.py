6f32b772d6df3e1e40f5f056d4f10b00750b4392
Brandon <byh2108@columbia.edu>
2016-10-02 16:01:24 -0400

    Display Menu
    Now able to edit menu as admin and view menu items on a webpage.

--- click_eat/menu/views.py
+++ click_eat/menu/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 def index(request):
-    return HttpResponse("Welcome to the menu.")
+    item_list = Item.objects.all()
+    output = ''
+    for i in item_list:
+        output = output + i.item_name + ' ' + str(i.item_price) + '<br/>'
+    return HttpResponse(output)
