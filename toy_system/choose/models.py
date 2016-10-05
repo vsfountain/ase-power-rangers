794da0d9e4e6c9a54b2d3abc5e75b930f868f249
applekuo92 <applekuo92@gmail.com>
2016-10-04 22:25:55 -0400

    unit test and static test

--- toy_system/choose/models.py
+++ toy_system/choose/models.py
@@ -5,21 +5,24 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Menu(models.Model):
+    '''Menu class is for dish object'''
     name = models.CharField(max_length=20)
     cost = models.DecimalField(max_digits=5, decimal_places=2)
     def __str__(self):
-    	return self.name
+        return self.name
 
 @python_2_unicode_compatible
 class CustRecord(models.Model):
-	name = models.CharField(max_length=20)
-	time = models.DateTimeField()
-	total_price = models.DecimalField(max_digits=6, decimal_places=2,default= 0)
-	payment = models.BooleanField(default= False)
-	def __str__(self):
-		return self.name
+    '''Each order is a CustRecord object'''
+    name = models.CharField(max_length=20)
+    time = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    payment = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
 
 class OrderRecord(models.Model):
-	cust_id = models.ForeignKey(CustRecord, on_delete=models.CASCADE)
-	dish_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
-	served = models.BooleanField(default= False)
+    '''Each dish in the order is an OrderRecord object'''
+    cust_id = models.ForeignKey(CustRecord, on_delete=models.CASCADE)
+    dish_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    served = models.BooleanField(default=False)
--- toy_system/choose/views.py
+++ toy_system/choose/views.py
@@ -1,44 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 #from django.template import loader
-from .models import Menu, CustRecord, OrderRecord
+
 from django.utils import timezone
 #from django.urls import reverse
 from django.core.urlresolvers import reverse
-
+from .models import Menu, CustRecord
 def index(request):
-	list_of_pie = Menu.objects.all()
-	#template = loader.get_template('choose/index.html')
-	context = {
-	    'list_of_pie': list_of_pie,
-	}
-	return render(request, 'choose/index.html', context)
-	#return HttpResponse(template.render(context, request))
-	#return HttpResponse(template.render(context, request))
+    '''index view'''
+    list_of_pie = Menu.objects.all()
+    context = {
+        'list_of_pie': list_of_pie,
+    }
+    return render(request, 'choose/index.html', context)
 
 def submit(request):
-	cust = CustRecord(name=request.POST["name"], time=timezone.now())
-	price = 0
-	cust.save()
-	menu = Menu.objects.all()
-	for dish in menu:
-		try:
-			tmp = request.POST['dish' + str(dish.id)]
-		#if tmp == "1":
-			price += dish.cost
-			cust.orderrecord_set.create(dish_id = dish)
-		except:
-			continue
-	cust.total_price = price
-	cust.save()
-	return HttpResponseRedirect(reverse('result', args=(cust.id,)))
+    '''submit view, the view after submission'''
+    cust = CustRecord(name=request.POST["name"], time=timezone.now())
+    price = 0
+    cust.save()
+    menu = Menu.objects.all()
+    for dish in menu:
+        try:
+            _ = request.POST['dish' + str(dish.id)]
+            price += dish.cost
+            cust.orderrecord_set.create(dish_id=dish)
+        except:
+            continue
+    cust.total_price = price
+    cust.save()
+    return HttpResponseRedirect(reverse('result', args=(cust.id,)))
 
 def result(request, cust_id):
+    '''result view, redirect from submit view, show result'''
     cust = get_object_or_404(CustRecord, pk=cust_id)
     cust_order = cust.orderrecord_set.all()
     menu_array = []
     for i in cust_order:
-    	menu_array.append(i.dish_id)
+        menu_array.append(i.dish_id)
     return render(request, 'choose/result.html', {'cust': cust, 'menu':menu_array})
